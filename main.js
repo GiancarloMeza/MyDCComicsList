32141bd20388acc082201a73a6fe8dbe0ee5f853
GiancarloMeza <giancameza@gmail.com>
2018-07-12 18:39:39 -0700

    Got names to display from API

---
+++ main.js
@@ -0,0 +1,12 @@
+var airtable_list_url = 'https://api.airtable.com/v0/apphqaKHaWm6NvtYO/Table%201?api_key=keyxoRy5JC73cJsWW'
+
+//This is where we get the JSON data from 
+$.getJSON(airtable_list_url, function(data) {
+    cinsle.log(data)
+    var items = [];
+    $.each( data.records, function(key, val){
+        console.log(val.fields)
+        items.push(`<h2>${val.fields['Name']}</h2>`);
+    });
+    $(".list-view").append(item.join(''));
+});
