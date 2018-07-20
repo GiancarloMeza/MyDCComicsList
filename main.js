c5a5592be34039ca123c550231c7b01c54bb3f63
GiancarloMeza <giancameza@gmail.com>
2018-07-20 01:36:09 -0700

    Added a navbar header and in the header contact info and what the website is about added an icon and the name of the website. In center on the page added the full name of the website and a description as to why i made this website. Added to button that do not have a use yet. In js added an updated card template so it looks better. In css added colors to everything, margins footers, headers, and a box-shadow. At the bottom of the page I added a button that will let users submit their favorite comic book stories (does not work yet)

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
 // This urls is from airtable from the Authentication section
 var airtable_list_url = 'https://api.airtable.com/v0/apphqaKHaWm6NvtYO/Table%201?api_key=keySx6GmwxJ6iqhTD';
 
+var cardTemplate = function(name, rating, pictures){
+    return `
+        <div class="row">
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top" src="${pictures}" alt="Card image cap">
+                    <div class="card-body">
+                        <p class="card-title">${name}</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                            <button type="button" class="btn btn-sm btn-outline-secondary">Get More Info</button>
+                            </div>
+                            <p class="card-text">${rating}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+}
+
 //This is where we get the JSON data from 
 $.getJSON( airtable_list_url, function( data ) {
     var items = [];
     $.each( data.records, function( key, val ) {
-        console.log(val.fields)
-        items.push(`<h2>${val.fields['Name']}</h2>`);
+        //console.log(val.fields)
+        var name = val.fields['Name'];
+        var rating = val.fields['Rating'];
+        var pictures = val.fields['Pictures'][0] ? val.fields['Pictures'][0].url : null;
+        var html = cardTemplate(name, rating, pictures);
+        items.push(html);
     });
     $(".list-view").append(items.join(''));
 });
