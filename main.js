40f85b089ecc4eb2b33dc9ccc3043ff94fcfed00
GiancarloMeza <giancameza@gmail.com>
2018-08-02 14:54:12 -0700

    added an id to name, made it bold and italicized. Fixed release date typo. Added more info to detailView.

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ var listView = function(id, name, rating, pictures){
         <div class="card mb-4 box-shadow">
         <a href="?id=${id}"><img height="550" class="card-img-top" src="${pictures}" alt="Card image cap"></a>
             <div class="card-body">
-                <p class="card-title">${name}</p>
+                <p id="name" class="card-title"><strong><i>${name}</i></strong></p>
                 <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
                     <a href="?id=${id}"><button type="button" class="btn btn-sm btn-outline-secondary">Get More Info</button></a>
@@ -44,18 +44,57 @@ var getDataForList = function(){
     });
 }
 
-var detailView = function(id, name, pictures, rating, cost, dccomics, buyit, about, realesdate, enddate, art, writtenby){
+var detailView = function(id, name, pictures, rating, cost, dccomics, buyit, about, releasedate, enddate, art, writtenby){
     return `
-  <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12"> 
-        <div class="card mb-4 box-shadow">
-        <a href="?id=${id}"><img height="550" class="card-img-top" src="${pictures}" alt="Card image cap"></a>
-            <div class="card-body">
-                <p class="card-title">${name}</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                    <a href="?id=${id}"><button type="button" class="btn btn-sm btn-outline-secondary">Get More Info</button></a>
+    <div id="wrapper">
+        <div id="about">
+            <h1 id="aboutHeader">About</h1>
+            <hr>
+            <p>${about}</p>
+        </div>
+    
+        <div id="card" class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+            <div class="card mb-4 box-shadow">
+                <img height="550" class="card-img-top" src="${pictures}" alt="Card image cap">
+                <div class="card-body">
+                    <p id="name" class="card-title"><strong><i>${name}</i></strong</p>
+                    <hr>
+                    <p id="rating" class="card-text">${rating}</p>
+                    <hr>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="card-text">
+                            <p id="runDate">Run Date:<p>
+                        </div>
+                        <p id="date" class="card-text">${releasedate} - ${enddate}</p>        
+                    </div>        
+                    <hr>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="card-text">
+                            <p id="price">Price:<p>
+                        </div>
+                        <a href="${buyit}"><p id="priceInfo" class="card-text">$${cost}</p></a>
                     </div>
-                    <p class="card-text">${rating}</p>
+                    <hr>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="card-text">
+                            <p id="writtenBy">Written By:<p>
+                        </div>
+                        <p id="writtenVar" class="card-text">${writtenby}</p>
+                    </div>
+                    <hr>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="card-text">
+                            <p id="artBy">Art By:<p>
+                        </div>
+                        <p id="artVar" class="card-text">${art}</p>
+                    </div>
+                    <hr>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="card-text">
+                            <p id="owner">Owner:<p>
+                        </div>
+                        <a href="${dccomics}"><p id="dcComics" class="card-text">DC Comics</p></a>
+                    </div>
                 </div>
             </div>
         </div>
@@ -77,12 +116,12 @@ var getDataForId = function(id) {
         var dccomics = fields["DCComics"];
         var buyit = fields["BuyIt"];
         var about = fields["About"];
-        var realesdate = fields["RealesDate"];
+        var releasedate = fields["ReleaseDate"];
         var enddate = fields["EndDate"];
         var art = fields["Art"];
         var writtenby = fields["WrittenBy"];
   
-        var itemHTML = detailView(id, name, pictures, rating, cost,  dccomics, buyit, about, realesdate, enddate, art, writtenby);
+        var itemHTML = detailView(id, name, pictures, rating, cost,  dccomics, buyit, about, releasedate, enddate, art, writtenby);
         html.push(itemHTML);
       html.push('</div>');
       $(".detail-view").append(html.join(""));
@@ -95,23 +134,3 @@ if (id) {
 } else {
     getDataForList();
 }
-
-// function myFunction() {
-//     var input, filter, myTable, myContainer, myListView;
-//     input = document.getElementById("myInput");
-//     filter = input.value.toUpperCase();
-//     table = document.getElementById("myTable");
-//     container = myTable.getElementById("myContainer");
-//     listView = myContainer.getElementById("myListView");
-  
-//     for (i = 0; i < tr.length; i++) {
-//       listView = listView[i].getElementsById("myListView")[0];
-//       if (listView) {
-//         if (listView.innerHTML.toUpperCase().indexOf(filter) > -1) {
-//           listView[i].style.display = "";
-//         } else {
-//           listView[i].style.display = "none";
-//         }
-//       } 
-//     }
-// }
